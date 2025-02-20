13c2f606dc4cf92bde6fe2df508fc45689ce7a2d
anthonyg771 <kamillavitorianascimentodossan@gmail.com>
2025-02-20 11:28:15 -0300

    0ijg

---
+++ BackSRC/CategoriaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace almoxarifadofront.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var url = "";
+
+            using HttpClient client = new HttpClient();
+            try
+            {
+                HttpResponseMessage response = client.GetAsync(url).Result;
+                response.EnsureSuccessStatusCode();
+                string json = response.Content.ReadAsStringAsync().Result;
+
+            }
+            catch (Exception) 
+            {
+                return View();
+            }
+            return View();
+        }
+        
+    }
+}
